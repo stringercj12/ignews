d375daba41942e6103be7fbff071dbf38dbbb8d4
Jefferson Clementino Ferreira <43593688+stringercj12@users.noreply.github.com>
2021-12-06 23:03:54 -0300

    Component Header

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,400&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Header } from "../components/Header";
+import styles from "./styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+   
+    </>
+  )
+}
